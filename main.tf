9c83371b9fa2d140e0382243d6ae29b355fb651b
EC2 Default User <ec2-user@ip-172-31-75-83.ec2.internal>
2026-04-30 01:18:03 +0000

    Add das vulnerabilidades

--- main.tf
+++ main.tf
@@ -9,6 +9,23 @@ resource "aws_vpc" "terraform_vpc" {
   }
 }
 
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::clc15-williamlima-terraform"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.terraform_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
+
+
 resource "aws_subnet" "subnet_public_1a" {
   vpc_id     = aws_vpc.terraform_vpc.id
   cidr_block = "10.0.1.0/24"
